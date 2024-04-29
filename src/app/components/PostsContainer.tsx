7461ee580be1feb1237e30d565f3af20d972a0ef
Hannah <hannah.v.d.wijngaard@gmail.com>
2024-04-29 09:13:01 +0100

    update cache results

--- src/app/components/PostsContainer.tsx
+++ src/app/components/PostsContainer.tsx
@@ -1,7 +1,9 @@
 import { db } from "../lib/db";
 import PostCard from "./PostCard";
+import { unstable_noStore as noStore } from "next/cache";
 
 const getPosts = async () => {
+  noStore();
   const response = await db.post.findMany({
     select: {
       id: true,
@@ -18,6 +20,7 @@ const getPosts = async () => {
 };
 
 const getReplies = async () => {
+  noStore();
   const response = await db.reply.findMany({
     select: {
       id: true,
@@ -33,7 +36,6 @@ const getReplies = async () => {
 };
 
 const PostsContainer = async () => {
-  // COMING FROM THE SERVER
   const posts = await getPosts();
   const replies = await getReplies();
 
