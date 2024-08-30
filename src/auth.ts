734f38465c310002f32e6e355a3da3f8b84ddc33
Hannah <hannah.v.d.wijngaard@gmail.com>
2024-08-30 09:38:36 +0100

    tweak

--- src/auth.ts
+++ src/auth.ts
@@ -13,17 +13,17 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       from: "onboarding@resend.dev",
     }),
 
-    nodemailer({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
-        auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD,
-        },
-      },
-      from: process.env.EMAIL_FROM,
-    }),
+    // nodemailer({
+    //   server: {`
+    //     host: process.env.EMAIL_SERVER_HOST,
+    //     port: process.env.EMAIL_SERVER_PORT,
+    //     auth: {
+    //       user: process.env.EMAIL_SERVER_USER,
+    //       pass: process.env.EMAIL_SERVER_PASSWORD,
+    //     },
+    //   },
+    //   from: process.env.EMAIL_FROM,
+    // }),
   ],
   secret: process.env.AUTH_SECRET,
   session: {
